9bd4032be703c37024000f58514aaea0e9dff347
pavaa <pavaa@PaviArul>
2025-08-08 13:49:22 +0530

    project

---
+++ springcore1/src/main/java/com/tnsif/springcore1/Bike.java
@@ -0,0 +1,13 @@
+package com.tnsif.springcore1;
+
+public class Bike implements vehicle{
+
+	@Override
+	public void ride() {
+		// TODO Auto-generated method stub
+		System.out.println("Bike is riding...");
+	}
+	
+	
+
+}
